dce80101cc3d31881daf075f2960b709cf181c8a
Kody M <kodylmontes@gmail.com>
2024-06-23 21:55:00 -0700

    added node removal to doubly linked list.

--- JS_snippets/doubly_linked_list_unsorted.js
+++ JS_snippets/doubly_linked_list_unsorted.js
@@ -30,6 +30,48 @@ class DoublyLinkedListUnsorted {
     this.tail = nodeToAdd;
     this.length++;
     return nodeToAdd;
+  }
 
+  removeSingleMatchingNode(value) {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    if (this.length === 1 && this.head.value === value) {
+      const nodeToReturn = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return nodeToReturn;
+    }
+
+    if (this.head.value === value) {
+      const nodeToReturn = this.head;
+      this.head = this.head.next;
+      this.head.previous = null;
+      this.length--;
+      return nodeToReturn;
+    }
+
+    if (this.tail.value === value) {
+      const nodeToReturn = this.tail;
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+      this.length--;
+      return nodeToReturn;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        const nodeToReturn = currentNode;
+        currentNode.prev = currentNode.next;
+        this.length--;
+        return nodeToReturn;
+      }
+      currentNode = currentNode.next;
+    }
+    return undefined;
   }
 }
